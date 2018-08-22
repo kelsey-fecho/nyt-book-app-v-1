6215b4cb3f5baad938b2e94469012946d475c8f9
Kelsey Fecho <kfecho30@gmail.com>
2018-08-22 17:53:55 -0400

    Remove react default css and add reference to Opps container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './App.css';
+import Opportunities from './Opportunities'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Opportunities />
         </header>
       </div>
     );
