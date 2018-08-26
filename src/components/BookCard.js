f326950f414d384a25af8fc7b051233613225e9b
Kelsey Fecho <kfecho30@gmail.com>
2018-08-26 16:21:42 -0400

    Remove unused Card Image field

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap';
+import { Card, CardText, CardBody, CardTitle, Button } from 'reactstrap';
 
 
 const BookCard = ({book}) =>
   <div>
     <Card>
-    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
     <CardBody>
       <CardTitle>{book.title}</CardTitle>
       <CardText>{book.desc}</CardText>
