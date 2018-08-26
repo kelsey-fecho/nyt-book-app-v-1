ecdb34c12506720b0f26defb3785e0b7561cb521
Kelsey Fecho <kfecho30@gmail.com>
2018-08-26 16:05:01 -0400

    Replace old references from opps to books

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap';
 
 
-const BookCard = ({opp}) =>
+const BookCard = ({book}) =>
   <div>
     <Card>
     <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
     <CardBody>
       <CardTitle>{book.title}</CardTitle>
       <CardText>{book.desc}</CardText>
-      <Button>Apply</Button>
+      <Button>Read</Button>
     </CardBody>
     </Card>
   </div>
