4b87e023d2ae6cd3e4dc3ab791f7bd5f9ff6fbd8
Kelsey Fecho <kfecho30@gmail.com>
2018-08-26 16:43:32 -0400

    Style book cards

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Card, CardText, CardBody, CardTitle, Button } from 'reactstrap';
+import { Card, CardText, CardSubtitle, CardBody, CardTitle, Button } from 'reactstrap';
 
 
 const BookCard = ({book}) =>
   <div>
-    <Card>
+    <Card inverse style={{ backgroundColor: '#d5beae', margin: '1.25rem auto' }}>
     <CardBody>
       <CardTitle>{book.title}</CardTitle>
+      <CardSubtitle>{book.author}</CardSubtitle>
       <CardText>{book.desc}</CardText>
       <Button>Read</Button>
     </CardBody>
