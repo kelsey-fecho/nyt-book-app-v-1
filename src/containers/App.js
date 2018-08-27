329450645bc3a91eb32dd50c466cede67f849a22
Kelsey Fecho <kfecho30@gmail.com>
2018-08-27 09:06:47 -0400

    Dismantle and delete search component:

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Books from './Books'
-import BookSearch from '../components/BookSearch'
 
 class App extends Component {
   constructor(){
@@ -11,24 +10,19 @@ class App extends Component {
   }
 
   componentDidMount(){
-    this.handleSubmit()
-  }
-
-  handleSubmit = () => {
-     fetch('https://api.nytimes.com/svc/books/v3/lists.json?list=combined-print-and-e-book-fiction&api-key=e6b608fbf6e6484bab1b9663f82858e0')
-      .then(res => res.json())
-      .then(({results}) => this.setState({
-        books: results.map(book => ({title: book.book_details[0].title,
-                                    desc: book.book_details[0].description,
-                                    author: book.book_details[0].author}))
-      }))
+    fetch('https://api.nytimes.com/svc/books/v3/lists.json?list=combined-print-and-e-book-fiction&api-key=e6b608fbf6e6484bab1b9663f82858e0')
+     .then(res => res.json())
+     .then(({results}) => this.setState({
+       books: results.map(book => ({title: book.book_details[0].title,
+                                   desc: book.book_details[0].description,
+                                   author: book.book_details[0].author}))
+     }))
   }
 
   render() {
     return (
       <div className="App">
           <Books books={this.state.books}/>
-          <BookSearch onSubmit={this.handleSubmit} />
       </div>
     );
   }
