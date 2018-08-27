922d6eca1492983a7515ef9f9bd501ada834f283
Kelsey Fecho <kfecho30@gmail.com>
2018-08-27 17:06:01 -0400

    Abstract away book fetch actions to their own file

---
+++ src/actions/bookActions.js
@@ -0,0 +1,11 @@
+const API_KEY =`${process.env.REACT_APP_NYT_API_KEY}`
+
+export function fetchFiction(){
+  return (dispatch) => {
+    dispatch({type: 'LOADING'});
+    return fetch(`https://api.nytimes.com/svc/books/v3/lists.json?list=combined-print-and-e-book-fiction&api-key=${API_KEY}`)
+     .then(res => {return res.json()})
+     .then(({results}) => {dispatch({type: 'FETCH_FICTION', payload: results})
+   })
+  }
+}
