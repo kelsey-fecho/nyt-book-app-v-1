4be1a324169487d68b32a99585c1ef983973475f
Kelsey Fecho <kfecho30@gmail.com>
2018-08-28 11:36:00 -0400

    Create shell for new comment form component

---
+++ src/components/CommentForm.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import { Form, Label, Input, FormText } from 'reactstrap';
+
+export default class CommentForm extends Component {
+  render(){
+    return(
+      
+    )
+  }
+}
