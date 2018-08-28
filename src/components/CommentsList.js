995db6a138717a65a9609024204374487d6d0150
Kelsey Fecho <kfecho30@gmail.com>
2018-08-28 12:50:09 -0400

    Add logic for looping through all comments provided from props in Comments

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -5,7 +5,13 @@ import Comment from '../components/Comment'
 export default CommentsList extends Component{
   render(){
     return(
-      
+      <Container>
+        <Row>
+          {this.props.comments.map((comment, index) =>
+            <Col md={{ ize: 8, offset: 2}}><Comment comment={comment} key={index} /></Col>
+          )}
+        </Row>
+      </Container>
     )
   }
 }
