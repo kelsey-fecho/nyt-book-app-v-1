a0cb183a8cd2caa7241822027e92c05ce005f863
Kelsey Fecho <kfecho30@gmail.com>
2018-08-28 12:59:47 -0400

    Remove unnessary React Component import

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-const Comment = ({comment}) => {
+const Comment = ({comment}) =>
   <div className="border rounded align-middle">
     <p>{comment.text}</p>
     <p><i>{comment.author}</i></p>
   </div>
-}
+
 
 export default Comment
