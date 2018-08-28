3fe723df6dd27a112026d13a4722e60dabca949f
Kelsey Fecho <kfecho30@gmail.com>
2018-08-28 11:36:36 -0400

    Create shell for comments list component

---
+++ src/components/CommentsList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {Container, Row, Col} from 'reactstrap'
+import Comment from '../components/Comment'
+
+export default CommentsList extends Component{
+  render(){
+    return(
+      
+    )
+  }
+}
