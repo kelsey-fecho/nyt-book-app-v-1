1305720e1c348fadf9e8f0e249a409b04d2ffbbd
Kelsey Fecho <kfecho30@gmail.com>
2018-08-29 11:10:05 -0400

    Add Logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import {Container, Row, Col} from 'reactstrap'
+import logo from '../images/Book_Ballyhoo.png'
 
 const NavBar = () => {
   return (
     <Container>
       <Row>
-        <Col sm="4"><NavLink to="/" exact className="nav-link">BookLister</NavLink></Col>
+        <Col><NavLink to="/" exact className="nav-link"><img src={logo} width="500" alt="logo"/></NavLink></Col>
+      </Row>
+      <Row>
         <Col sm="4"><NavLink to="/fiction" exact className="nav-link">Combined Fiction</NavLink></Col>
         <Col sm="4"><NavLink to="/nonfiction" exact className="nav-link">Combined Non-Fiction</NavLink></Col>
       </Row>
---
+++ src/images/index.js
@@ -0,0 +1,1 @@
+export logo from './Book_Ballyhoo.png'
