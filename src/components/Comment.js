84246ae457b516a021a0b6dab7b900226194fe78
Kelsey Fecho <kfecho30@gmail.com>
2018-08-29 10:17:57 -0400

    Align comment properties with those defined in CommentForm

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Comment = ({comment}) =>
   <div className="border rounded align-middle">
-    <p>{comment.text}</p>
+    <p>{comment.comment}</p>
     <p><i>{comment.author}</i></p>
   </div>
 
