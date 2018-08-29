9f9b95f50fc841a36f29785f0db8e2438869f1fb
Kelsey Fecho <kfecho30@gmail.com>
2018-08-29 11:21:37 -0400

    Style Comment Form component

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -29,22 +29,21 @@ export class CommentForm extends Component {
 
   render(){
     return(
-      <Container style={{margin: "1em"}}>
+      <Container>
         <Row>
-          <Col md={{ ize: 8, offset: 2}}>
+          <Col md={{ size: 11, offset: 2}} style={{margin: "1em", border: "1px solid #6c757d", padding: "1em"}}>
             <form onSubmit={this.handleSubmit}>
+              <h4 style={{color: '#6c757d'}}>Let Us Know What You Think</h4>
               <div>
-                <label>Your Name
-                  <input type='text' name='author' id='newauthor'onChange={this.handleChange} value={this.state.author}/>
-                </label>
+                <input type='text' name='author' id='newauthor'onChange={this.handleChange} value={this.state.author} placeholder="Your Name"/>
               </div>
+              <br/>
               <div>
-                <label>Your Comment
-                  <input type='textarea' name='comment' id='newtext' onChange={this.handleChange} value={this.state.comment}/>
-                </label>
+                <textarea rows='5' cols='80' name='comment' id='newtext' onChange={this.handleChange} value={this.state.comment} placeholder="All Your Thoughts Here"/>
               </div>
+              <br/>
               <div>
-                <input type="submit"/>
+                <input type="submit" className="btn btn-secondary"/>
               </div>
             </form>
           </Col>
